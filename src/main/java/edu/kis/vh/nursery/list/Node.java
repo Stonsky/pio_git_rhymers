12fffccb64598dc6d83faf8a962a048e84495b22
Stonsky <66369641+Stonsky@users.noreply.github.com>
2021-01-12 18:24:39 +0100

    encapsulate fields

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,11 +2,35 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-    public int value;
-    public Node prev, next;
+    private int value;
+    private Node prev;
+    private Node next;
 
     public Node(int i) {
-        value = i;
+        setValue(i);
     }
 
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public edu.kis.vh.nursery.list.Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(edu.kis.vh.nursery.list.Node prev) {
+        this.prev = prev;
+    }
+
+    public edu.kis.vh.nursery.list.Node getNext() {
+        return next;
+    }
+
+    public void setNext(edu.kis.vh.nursery.list.Node next) {
+        this.next = next;
+    }
 }
